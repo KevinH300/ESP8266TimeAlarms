abcd523c5decc173646737d6e30ac564d3a40605
Kevin Hofmans <kevin@bluemail.be>
2023-11-19 20:11:08 +0100

    Update definition check

--- ESP8266TimeAlarms.h
+++ ESP8266TimeAlarms.h
@@ -1,6 +1,5 @@
 //  TimeAlarms.h - Arduino Time alarms header for use with Time library
 
-
 #ifndef TimeAlarms_h
 #define TimeAlarms_h
 
@@ -18,36 +17,38 @@
 #endif
 
 #define USE_SPECIALIST_METHODS  // define this for testing
-#if !defined(timeDayOfWeek_t)
+#ifndef _Time_h
   typedef enum {
       TA_dowInvalid, TA_dowSunday, TA_dowMonday, TA_dowTuesday, TA_dowWednesday, TA_dowThursday, TA_dowFriday, TA_dowSaturday
   } timeDayOfWeek_t;
-#endif
-#define SECS_PER_MIN  ((time_t)(60UL))
-#define SECS_PER_HOUR ((time_t)(3600UL))
-#define SECS_PER_DAY  ((time_t)(SECS_PER_HOUR * 24UL))
-#define DAYS_PER_WEEK ((time_t)(7UL))
-#define SECS_PER_WEEK ((time_t)(SECS_PER_DAY * DAYS_PER_WEEK))
 
-#define SECS_PER_YEAR ((time_t)(SECS_PER_DAY * 365UL)) // TODO: ought to handle leap years
+  #define SECS_PER_MIN  ((time_t)(60UL))
+  #define SECS_PER_HOUR ((time_t)(3600UL))
+  #define SECS_PER_DAY  ((time_t)(SECS_PER_HOUR * 24UL))
+  #define DAYS_PER_WEEK ((time_t)(7UL))
+  #define SECS_PER_WEEK ((time_t)(SECS_PER_DAY * DAYS_PER_WEEK))
+  #define SECS_PER_YEAR ((time_t)(SECS_PER_DAY * 365UL)) // TODO: ought to handle leap years
+#endif
 
 #define secs_per_year(y) ((time_t)(SECS_PER_DAY * year_lengths[isleap(y)]))
 #define isleap(y) ((((y) % 4) == 0 && ((y) % 100) != 0) || ((y) % 400) == 0)
 
-/* Useful Macros for getting elapsed time */
-#define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)  
-#define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN) 
-#define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
-#define dayOfWeek(_time_)  ((( _time_ / SECS_PER_DAY + 4)  % DAYS_PER_WEEK)+1) // 1 = Sunday
-#define elapsedDays(_time_) ( _time_ / SECS_PER_DAY)  // this is number of days since Jan 1 1970
-#define elapsedSecsToday(_time_)  (_time_ % SECS_PER_DAY)   // the number of seconds since last midnight 
-// The following macros are used in calculating alarms and assume the clock is set to a date later than Jan 1 1971
-// Always set the correct time before settting alarms
-#define previousMidnight(_time_) (( _time_ / SECS_PER_DAY) * SECS_PER_DAY)  // time at the start of the given day
-#define nextMidnight(_time_) ( previousMidnight(_time_)  + SECS_PER_DAY )   // time at the end of the given day 
-#define elapsedSecsThisWeek(_time_)  (elapsedSecsToday(_time_) +  ((dayOfWeek(_time_)-1) * SECS_PER_DAY) )   // note that week starts on day 1
-#define previousSunday(_time_)  (_time_ - elapsedSecsThisWeek(_time_))      // time at the start of the week for the given time
-#define nextSunday(_time_) ( previousSunday(_time_)+SECS_PER_WEEK)          // time at the end of the week for the given time
+#ifndef _Time_h
+  /* Useful Macros for getting elapsed time */
+  #define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)  
+  #define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN) 
+  #define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
+  #define dayOfWeek(_time_)  ((( _time_ / SECS_PER_DAY + 4)  % DAYS_PER_WEEK)+1) // 1 = Sunday
+  #define elapsedDays(_time_) ( _time_ / SECS_PER_DAY)  // this is number of days since Jan 1 1970
+  #define elapsedSecsToday(_time_)  (_time_ % SECS_PER_DAY)   // the number of seconds since last midnight 
+  // The following macros are used in calculating alarms and assume the clock is set to a date later than Jan 1 1971
+  // Always set the correct time before settting alarms
+  #define previousMidnight(_time_) (( _time_ / SECS_PER_DAY) * SECS_PER_DAY)  // time at the start of the given day
+  #define nextMidnight(_time_) ( previousMidnight(_time_)  + SECS_PER_DAY )   // time at the end of the given day 
+  #define elapsedSecsThisWeek(_time_)  (elapsedSecsToday(_time_) +  ((dayOfWeek(_time_)-1) * SECS_PER_DAY) )   // note that week starts on day 1
+  #define previousSunday(_time_)  (_time_ - elapsedSecsThisWeek(_time_))      // time at the start of the week for the given time
+  #define nextSunday(_time_) ( previousSunday(_time_)+SECS_PER_WEEK)          // time at the end of the week for the given time
+#endif
 
 static const int year_lengths[2] = {
   365,
